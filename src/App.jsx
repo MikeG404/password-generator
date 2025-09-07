a4e33ca82bd998c39e6516b024dba68af2000557
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-09-07 13:00:28 +0200

    create Slider functionnality with min, max and currentValue

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Slider from './components/Slider'
 
 function App() {
 
   return (
     <main>
-        <h1 class="text-3xl font-bold underline bg-amber-400">    Hello world!  </h1>
+        
+        <Slider />
     </main>
   )
 }
---
+++ src/components/Slider.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+export default function Slider() {
+    const [values, setValues] = useState({
+        min: 0,
+        max: 20,
+        currentValue: 10
+    })
+
+    const handleChange = (e) => {
+        setValues({
+            ...values,
+            currentValue: Number(e.target.value)
+        })
+    }
+
+  return (
+    <div id='slider'>
+        <div id='slider-text' className='flex w-full'>
+            <label htmlFor="slider">Character Length</label>
+            <p>{values.currentValue}</p>
+        </div>
+        <input 
+            type='range' 
+            name='slider' 
+            step='1' 
+            min={values.min} 
+            max={values.max} 
+            value={values.currentValue} 
+            onChange={handleChange}/>
+    </div>
+  )
+}
