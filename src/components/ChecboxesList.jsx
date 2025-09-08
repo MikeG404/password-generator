fe749123af1eee5698081b1276ddda2bb3edfb6c
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-09-08 07:05:20 +0200

    Create checkbox style without check inside on click

--- src/components/ChecboxesList.jsx
+++ src/components/ChecboxesList.jsx
@@ -3,11 +3,23 @@ import React from 'react'
 export default function ChecboxesList() {
   return (
     <div>
-        <ul>
-            <li><input type="checkbox" />Include Uppercase Letters</li>
-            <li><input type="checkbox" />Include Lowercase Letters</li>
-            <li><input type="checkbox" />Include Numbers</li>
-            <li><input type="checkbox" />Include Symbols</li>
+        <ul className='flex flex-col gap-4 preset-4 text-gray-200-2'>
+            <li>
+                <input type="checkbox" className='checkbox' name='checkbox-1'/>
+                <label htmlFor="checkbox-1">Include Uppercase Letters</label>
+            </li>
+            <li>
+                <input type="checkbox" className='checkbox' name='checkbox-1'/>
+                <label htmlFor="checkbox-1">Include Lowercase Letters</label>
+            </li>
+            <li>
+                <input type="checkbox" className='checkbox' name='checkbox-1'/>
+                <label htmlFor="checkbox-1">Include Numbers</label>
+            </li>
+            <li>
+                <input type="checkbox" className='checkbox' name='checkbox-1'/>
+                <label htmlFor="checkbox-1">Include Symbols</label>
+            </li>   
         </ul>
     </div>
   )
