245083597c308fa555bd402a74b3596ba14ce8e9
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-09-08 06:26:45 +0200

    Create SIMPLE checboxes list components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import ChecboxesList from './components/ChecboxesList'
 import PasswordInput from './components/PasswordInput'
 import Slider from './components/Slider'
 import Strength from './components/Strength'
@@ -6,12 +7,13 @@ import Strength from './components/Strength'
 function App() {
 
   return (
-    <main className='h-screen flex flex-col justify-center items-center gap-4 p-6 '>
-      <h1 className='preset-4'>Password Generator</h1>
+    <main className='h-screen flex flex-col justify-center items-center gap-4 p-6 bg-gradient-black'>
+      <h1 className='preset-4 text-gray-600-2'>Password Generator</h1>
       {/* INPUT PASSWORD */}
       <PasswordInput />
-      <section className='w-full flex flex-col p-4 bg-gray-800-2'>
+      <section className='w-full flex flex-col p-4 gap-8 bg-gray-800-2'>
         <Slider />
+        <ChecboxesList />
         {/* <Strength /> */}
 
       </section>
---
+++ src/components/ChecboxesList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function ChecboxesList() {
+  return (
+    <div>
+        <ul>
+            <li><input type="checkbox" />Include Uppercase Letters</li>
+            <li><input type="checkbox" />Include Lowercase Letters</li>
+            <li><input type="checkbox" />Include Numbers</li>
+            <li><input type="checkbox" />Include Symbols</li>
+        </ul>
+    </div>
+  )
+}
